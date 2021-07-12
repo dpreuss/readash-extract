f39f7498c8a4fe001f8271b3ca86246a3a10cb7b
donp <dpreuss@starfishstorage.com>
2021-07-12 04:48:42 -0400

    lastest update

--- redash-extract-dash.py
+++ redash-extract-dash.py
@@ -7,8 +7,8 @@
 
 # TODO: pass in key (or log in)
 #   Get order to work correctly
-#   Put dashboard in separate dir and create tar file of dashboard
 #   Fix missing parameter size_gb issue for problem-areas dashboard
+#   Does not support dashboard parameters with multiple queries
 
 import os
 import ssl
@@ -82,7 +82,6 @@ def extract_visualization(viz, qdata, query_id):
 
     order = viz['options']['position']['row'] * 10 + viz['options']['position']['col']
 
-
     if v['type'] == "TABLE":
 	   meta['queries'].append((order, qdata))
 	   return
@@ -106,28 +105,46 @@ def extract_visualization(viz, qdata, query_id):
                 "options": v['options'],
                 "type": v['type']
     }
-    if 'width' in viz:
-	    local_data['width'] = viz['width']
-	    data['widget_width'] = "half"
+#    if viz['options']['position']['sizeX'] < 4 : 
+#	    local_data['width'] = viz['width']
+#	    data['widget_width'] = "half"
 
-    if 'parameterMappings' in viz['options']:
+    if 'parameters' in v['query']['options'] and len(v['query']['options']['parameters']) >0:
+        # print v['query']['options']['parameters']
 
-        for param, val in viz['options']['parameterMappings'].items():
+        for param in v['query']['options']['parameters']:
 
             pardata = {
-                "title": param,
-                "name":  val['name'],
-                "value": val['value'],
+                "title": param['title'],
+                "name":  param['name'],
+                "value": param['value'],
                 "type": "number"
             }
             if pardata['name'] == "volume":
                 pardata['type'] = "text"
-            if val['type'] == "widget-level":
+            if param['type'] == "widget-level":
                 pardata['global'] = False
             else:
                 pardata['global'] = True
 
             par_arr.append(pardata)
+
+
+#        if 'parameterMappings' in viz['options']:
+# Never entered
+
+#            for param, val in viz['options']['parameterMappings'].items():
+
+#                parmap = {
+#                    "title": param,
+#                    "name":  val['name'],
+#                    "value": pardata['value'],
+#                    "type": "number"
+#                }
+#                if parmap['name'] == "volume":
+#                    pardata['type'] = "text"
+
+#                par_arr.append(parmap)
 	     
 
         local_data['parameters'] = par_arr
@@ -140,8 +157,6 @@ def extract_visualization(viz, qdata, query_id):
     local_data['visualization'] = data_arr
     meta['queries'].append((order, local_data))
 
-     #sql.close
-
 
 def import_dashboard(dashname, api_key):
 
@@ -153,6 +168,9 @@ def import_dashboard(dashname, api_key):
     except Exception as ex:
        print "Cannot connect, or no such dashboard: ", meta['dashboards']
        sys.exit(1)
+
+    # Echo dashboard
+    # print json.dumps(d, indent=3)
 
     data = {'name': d['name']}
 
@@ -176,7 +194,6 @@ def export_dashboard(dirpath):
     print "Exporting   " + dirpath
     first_item = True
 	
-
     with open(dirpath+'/'+dirpath+'.json', "w") as dash:
 	dash.write("[\n")
 	for query in sorted(meta['queries'], key=lambda _ordered_query_tuple: _ordered_query_tuple[0]):
